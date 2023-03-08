f2899feb34b57db34d54bf4a5808778206ce45da
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-08 00:23:20 +0000

    Update Home.js
    Content added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,12 +9,12 @@ const aabc  = {
 
 function Home() {
   return (
-    <div style={aabc} className=" container  py-5 bg-info bg-opacity-10 shadow-lg text-center min-vh-50 d-grid aabc">
+    <div style={aabc} className=" container py-5 bg-info bg-opacity-10 shadow-lg text-center min-vh-50 d-grid aabc">
 
       <h1 >Hello 
-        I'm Nicholas Martin
+        I'm Nicholas Martin.
       </h1>
-      <p>Web Dev</p>
+      <h5 className="text-decoration-underline mt-3">I am Web Dev enthusiastic about the Cloud. </h5>
       
       
     </div>
