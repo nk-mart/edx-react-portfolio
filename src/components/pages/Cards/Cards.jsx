b51bdc311946879156509a81f0c7fdb94494fc5b
nk-mart <113395569+nk-mart@users.noreply.github.com>
2023-03-12 23:49:51 +0000

    Cards Comp Updated
    Added map function to loop through Json data.

--- src/components/pages/Cards/Cards.jsx
+++ src/components/pages/Cards/Cards.jsx
@@ -3,23 +3,40 @@ import './style.css'
 
 export default function Cards(props){
  const project=props.projects
+    
 
-console.log(props.projects[0])
 
    return(
     
     <>
-    <div className="card">
+    
+
+       { project.map(projects =>{
+        return<>
+        
+        <div className="card">
         <div className="img-container">
+        <img src={projects.pic} alt="" className="img-fluid"/>
+        </div>
+        
+        <div className="card-body">
+            <h5 className="card-title">Project:{projects.name}</h5>
+            <div className="card-text">{projects.about}</div>
+        </div>
+        </div>
+        </>
+       })}
+        
+        {/* <div className="img-container">
         <img src={project[0].pic} alt="" className="img-fluid"/>
         </div>
         
         <div className="card-body">
-            <h5 className="card-title">Project: {project[0].name}</h5>
-            <div className="card-text">somekind of text</div>
-        </div>
+            <h5 className="card-title">Project:  {project[0].name}</h5>
+            <div className="card-text">{project[0].about}</div>
+        </div> */}
 
-    </div>
+    
     </>
     
 
